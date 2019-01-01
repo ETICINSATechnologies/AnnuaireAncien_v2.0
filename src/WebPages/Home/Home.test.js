dec988760c8745b616ed123e39589b6f7da8e876
laurenttainturier <laurent.tainturier.pro@gmail.com>
2019-01-01 14:02:13 +0100

    experiments with tests

--- src/WebPages/Home/Home.test.js
+++ src/WebPages/Home/Home.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Home';
+import Home from './Home';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({adapter: new Adapter()});
+
+describe('Home render tests', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Home/>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('Home unit test', () => {
+        expect(
+            Enzyme.shallow(<Home/>)
+            .find('div.Home')
+            .length
+        ).toBe(1)
+    });
+
+    it('Home snapshot test', () => {
+        const component = renderer.create(
+            <Home/>
+        );
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    })
 });
+
+
---
+++ src/__mocks__/fetch.js
@@ -0,0 +1,27 @@
+import member from "../Tools/member";
+import members from "../Tools/members";
+import board from "../Tools/board";
+
+export default (url) => {
+    let expectedResponse = null;
+
+    if (/auth\/login$/.test(url))
+        expectedResponse = {token: 'this is a test token'};
+    else if (/core\/member\/(\d*|me)$/.test(url))
+        expectedResponse = member;
+    else if (/core\/member\/board$/.test(url))
+        expectedResponse = board;
+    else if (/core\/member$/.test(url))
+        expectedResponse = members;
+    else
+        return null;
+
+    return Promise.resolve({
+        status: 200,
+        json: () => {
+            return Promise.resolve(
+                expectedResponse
+            )
+        }
+    })
+};
