ae40ed1a0dd90289a2cee10033bf176dd6071b4a
dabeliuteew <teck-wan.wong@etic-insa.com>
2019-05-03 23:42:41 +0200

    Implemented upload and download functionalities

--- src/WebPages/Data/Data.tsx
+++ src/WebPages/Data/Data.tsx
@@ -1,27 +1,116 @@
 import React, {Component} from 'react';
-import {Redirect} from 'react-router-dom';
 
 import './Data.css';
 import Header from "../../Components/Header/Header";
 import Nav from "../../Components/Nav/Nav";
 import Auth from "../../Components/Auth/Auth";
-import {emitKeypressEvents} from "readline";
 
 interface AdminState {
-
+    fileSpecified: boolean,
+    fileDragged: boolean,
+    fileName: string,
+    uploadRequested: boolean,
+    uploadSucceed: boolean,
+    downloadRequested: boolean,
+    downloadSucceed: boolean
 }
 
 class Data extends Component<{}, AdminState> {
+    state = {
+        fileSpecified: false,
+        fileDragged: false,
+        fileName: '',
+        uploadRequested : false,
+        uploadSucceed: false,
+        downloadRequested: false,
+        downloadSucceed: false
+    };
 
     dragOverHandler = (event: any) => {
-        console.log("it's here");
+        this.setState({
+            fileDragged: true
+        });
         event.preventDefault();
+    };
+
+    dragLeaveHandler = (event: any) => {
+        this.setState({
+            fileDragged: false
+        });
     }
 
     dropHandler = (event: any) => {
-        console.log("it fell");
+        let uploadInput = document.querySelector('#file-upload') as HTMLInputElement;
+        uploadInput.files = event.dataTransfer.files;
+        this.setState({
+            fileSpecified: true,
+            fileName: uploadInput.files![0].name
+        });
         event.preventDefault();
+    };
+
+    fileInputChangeHandler = (event: any) => {
+        this.setState({
+            fileSpecified: true,
+            fileName: event.target.files[0].name
+        });
     }
+
+    uploadClickHandler = () => {
+        let fileInput = (document.querySelector('#file-upload') as HTMLInputElement);
+        if (this.state.fileSpecified) {
+            const formData = new FormData();
+            formData.append('file', fileInput.files![0], this.state.fileName);
+            fetch(`/yearbook/upload`, {
+                method: `POST`,
+                headers:{
+                    Authorization: Auth.getToken(),
+                },
+                body: formData
+            })
+                .then(res => {
+                    if(res.status === 204){
+                        this.setState({
+                            uploadRequested: true,
+                            uploadSucceed: true
+                        })
+                    }else{
+                        this.setState({
+                            uploadRequested: true,
+                        })
+                    }
+                })
+        }else {
+            alert("Vous n'avez pas encore spécifié un fichier!");
+        }
+    }
+
+    downloadClickHandler = () => {
+        fetch(`/yearbook/download`, {
+            method: `GET`,
+            headers:{
+                Authorization: Auth.getToken()
+            }
+        }).then((res) => {
+                if(res.status === 200){
+                    this.setState({
+                        downloadRequested: true,
+                        downloadSucceed: true
+                    });
+                    let link = document.createElement('a');
+                    link.href = res.url;
+                    document.body.appendChild(link);
+                    link.click();
+                    document.body.removeChild(link);
+                }else{
+                    this.setState({
+                        downloadRequested: true,
+                        downloadSucceed: false
+                    });
+                }
+            }
+        )
+    };
 
     render() {
         let activeButton = ['home'];
@@ -41,16 +130,35 @@ class Data extends Component<{}, AdminState> {
             <section className="Data">
                 <div className='import'>
                     <h2>Importer la base de données de l'annuaire</h2>
-                    <h3>Attention! Cette action efface la base éxistante!</h3>
-                    <div id='drop_zone' onDrop={this.dropHandler} onDragOver={this.dragOverHandler}>
-                        <p>Copier votre fichier ici</p>
-                        <p> OU </p>
-                        <input type='file'/>
+                    {
+                        this.state.uploadRequested?
+                            this.state.uploadSucceed?
+                                <h3 className='affirmation'>La base est mise à jour.</h3>:
+                                <h3 className='warning'>Une erreur est survenue.</h3>:
+                            <h3 className='warning'>Attention! Cette action efface la base existante!</h3>
+                    }
+                    <div id='drop_zone' onDrop={this.dropHandler} onDragOver={this.dragOverHandler}
+                    onDragLeave={this.dragLeaveHandler}>
+                        {
+                        this.state.fileSpecified?
+                            <h2>{this.state.fileName}</h2>:
+                            this.state.fileDragged ?
+                                '':
+                                <div className='indication'>
+                                    <h3>Copier votre fichier ici</h3>
+                                    <h3> OU </h3>
+                                    <label className='upload' htmlFor='file-upload'>Choisir un fichier</label>
+                                </div>
+                        }
+                        <input id='file-upload' type='file' onChange={this.fileInputChangeHandler}/>
                     </div>
-                    <input type='button' value='Envoyer'/>
+                    <input type='button' value='Envoyer' onClick={this.uploadClickHandler}/>
                 </div>
                 <div className='export'>
-                    <input id='download' type='button' value="Télécharger l'Annuaire"/>
+                    <input id='download' type='button' value="Télécharger l'Annuaire" onClick={this.downloadClickHandler}/>
+                    {this.state.downloadRequested?
+                        this.state.downloadSucceed? '': <h3 className='warning'>Une erreur est survenue.</h3>
+                        :''}
                 </div>
             </section>
             </React.Fragment>
