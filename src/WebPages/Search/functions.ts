738cd3d613facdf8f95c27ce71a83f8867435cf4
Sadsitha Lokuge <slokuge97@gmail.com>
2020-10-05 01:11:36 +0200

    Added to do for export

--- src/WebPages/Search/functions.ts
+++ src/WebPages/Search/functions.ts
@@ -1,8 +1,9 @@
 import Auth from "../../Components/Auth/Auth";
 import { parse } from "json2csv";
 
+// To-do : Needs more complex page handling. Right now, will only download first page
 export const exportAllAlumni = () => {
-    const pageSize = 1000;
+    const pageSize = 25;
     const pageNumber = 0;
     let url: string = `${process.env.REACT_APP_BACKEND_URL}/api/v1/core/member?isAlumni=true&pageSize=${pageSize}&pageNumber=${pageNumber}`;
 
@@ -17,13 +18,12 @@ export const exportAllAlumni = () => {
                     const flattenedResults = result.content.map((item: any) => flattenObject(item));
                     try {
                         const csv = 'data:text/csv;charset=utf-8,' + parse(flattenedResults);
-                        console.log(csv)
                         const encodedUri = encodeURI(csv);
                         const link = document.createElement("a");
                         link.setAttribute("href", encodedUri);
                         link.setAttribute("download", "alumni_etic.csv");
                         document.body.appendChild(link); // Required for FF
-                        link.click(); // This will download the data file named "my_data.csv".
+                        link.click();
                     } catch (err) {
                         console.error(err);
                     }
